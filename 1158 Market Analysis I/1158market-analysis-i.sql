fb3fd6da0ef38c1a1ee2a21c372d0b4ae2fde75f
Pratima_Prasad <pprasa11@asu.edu>
2025-06-17 04:42:42 +0000

    Code challenge submission for question: 1158 Market Analysis I

---
+++ 1158 Market Analysis I/1158market-analysis-i.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+select
+u.user_id as 'buyer_id',
+u.join_date,
+count(case when year(o.order_date)='2019' then 1 end) as 'orders_in_2019'
+from Users as u
+left join Orders as o 
+on u.user_id=o.buyer_id
+group by u.user_id
