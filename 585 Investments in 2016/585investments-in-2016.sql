b29d7def809a8caa702f384be7f0957a860f909b
Pratima_Prasad <pprasa11@asu.edu>
2025-06-23 07:00:23 +0000

    Code challenge submission for question: 585 Investments in 2016

---
+++ 585 Investments in 2016/585investments-in-2016.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+select round(sum(tiv_2016),2) as tiv_2016
+from(
+    select *,
+    count(*) over(partition by tiv_2015) as tiv_cnt,
+    count(*) over(partition by lat, lon) as loc_cnt
+    from Insurance
+) t0
+where tiv_cnt>1 and loc_cnt=1
